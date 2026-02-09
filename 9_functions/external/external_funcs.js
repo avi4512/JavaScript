860c8a7f82f1f0d870bed2fac0c2b5c8854b86a3
avi4512 <avireddy4512@gmail.com>
2026-02-09 18:33:40 +0530

    .

---
+++ 9_functions/external/external_funcs.js
@@ -0,0 +1,12 @@
+
+function func1(){
+    document.write("This is Function1 from external JS file<br>")
+}
+
+function func2(){
+    document.write("This is Function2 from external JS file<br>")
+}
+
+function func3(){
+    alert("This is Function3 from external JS file")
+}
